6d5452547e90f7991cf18e050e4d04605cb57dc0
Wesleson22 <wesllesonsowza@gmail.com>
2022-06-21 19:38:22 -0300

    Componentes

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -2,8 +2,16 @@
 
 */
 import React from 'react';
-import {View} from 'react-native'
+import {View, Text} from 'react-native'
 
 import Primeiro from'./components/Primeiro'
+import CompPadrao, {Comp1, Comp2} from './components/Multi'
 
-export default ()=> <View><Primeiro/></View>
+export default ()=> (
+    <View>
+        <CompPadrao/>
+        <Comp1/>
+        <Comp2/>
+        <Primeiro/>
+    </View>
+)
---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+export default function Comp(){
+    return <Text>Com #01 Oficial</Text>
+}
+
+export function Comp1(){
+    return <Text>Com #02</Text>
+}
+
+export function Comp2(){
+    return <Text>Com #03</Text>
+
+}
+
